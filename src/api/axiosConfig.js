1a2145632b5d4d13b75ab17a383bb089a3eb8f4e
dexav <xnursiwat@gmail.com>
2026-05-14 23:03:54 +0200

    comment on axiosconfig

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080", //its localhost:8080, because teh axios shit doesnt really work for now and for local build doesnt even matter so its localhost:8080 for now.
   headers: { "ngrok-skip-browser-warning": "true" },
 });
